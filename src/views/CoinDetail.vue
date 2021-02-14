6bda8c7a7009ee823ca3c2f60e93e507aada2f55
cristianiniguez <felizpc1@gmail.com>
2021-02-14 19:35:47 -0400

    platzi exchange: mejorar proyecto - links y conversion

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center">
       <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
     </div>
-    <template v-if="!isLoading && asset.id">
+    <template v-if="!isLoading && asset">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -48,22 +48,27 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
-            Cambiar
+            {{ formUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                :placeholder="`Valor en ${formUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl">
+            {{ convertResult }} {{ formUsd ? asset.symbol : "USD" }}
+          </span>
         </div>
       </div>
 
@@ -119,11 +124,25 @@ export default {
       asset: {},
       history: [],
       markets: [],
-      isLoading: false
+      isLoading: false,
+      formUsd: true,
+      convertValue: null
     };
   },
 
   computed: {
+    convertResult() {
+      if (!this.convertValue) {
+        return 0;
+      }
+
+      const result = this.formUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd;
+
+      return result.toFixed(4);
+    },
+
     min() {
       return Math.min(
         ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
@@ -144,11 +163,21 @@ export default {
     }
   },
 
+  watch: {
+    $route() {
+      this.getCoin();
+    }
+  },
+
   created() {
     this.getCoin();
   },
 
   methods: {
+    toggleConverter() {
+      this.formUsd = !this.formUsd;
+    },
+
     getWebsite(exchange) {
       this.$set(exchange, "isLoading", true);
       api
@@ -163,6 +192,7 @@ export default {
 
     getCoin() {
       const id = this.$route.params.id;
+      console.log("hello");
       this.isLoading = true;
 
       Promise.all([
